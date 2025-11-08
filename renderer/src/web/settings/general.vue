cba71c9bc1d821ca9788eed45dbbd92e5d22df52
Lucas Chen <l_in1995@163.com>
2025-11-08 18:01:36 +0800

    兼容 intl 字段

--- renderer/src/web/settings/general.vue
+++ renderer/src/web/settings/general.vue
@@ -17,12 +17,8 @@
         <ui-radio v-model="realm" value="pc-garena">{{ t('Hotcool') }}</ui-radio>
       </div>
     </div>
-    <ui-checkbox class="mb-4" v-if="language !== 'en' && realm === 'pc-ggg'"
-      v-model="useIntlSite" :disabled="forcedIntlSite"
-      :class="{ 'text-gray-500': forcedIntlSite }">{{ t(':use_intl_site') }} <span class="bg-gray-200 text-gray-900 rounded px-1">www.pathofexile.com</span></ui-checkbox>
-    <div class="mb-4 mt-4">
-    <div class="mb-4" v-if="language === 'zh_CN'">
-      <div class="flex-1 mb-1">{{ t('Realm') }}</div>
+    <div class="mb-2" v-if="language === 'zh_CN'">
+      <div class="flex-1 mb-1">{{ t('realm') }}</div>
       <div class="flex gap-x-4">
         <ui-radio v-model="realm" value="pc-ggg">{{ t('realm_intl') }}</ui-radio>
         <ui-radio v-model="realm" value="pc-tencent">{{ t('realm_tencent') }}</ui-radio>
@@ -31,47 +27,54 @@
           <span><input v-model="cookies" class="rounded bg-gray-900 px-2 flex-1"></span>
         </div>
       </div>
-      <div class="flex-1 mt-2" v-show="realm === 'pc-tencent'">{{ t('sale_type') }}</div>
-      <div class="flex gap-x-4" v-show="realm === 'pc-tencent'">
-        <ui-radio v-model="saleType" value="any">{{ t('sale_type_any') }}</ui-radio>
-        <ui-radio v-model="saleType" value="strict_auto_buyout">{{ t('sale_type_strict_auto_buyout') }}</ui-radio>
-      </div>
     </div>
+    <ui-checkbox class="mb-4" v-if="language !== 'en' && realm === 'pc-ggg'"
+      v-model="useIntlSite" :disabled="forcedIntlSite"
+      :class="{ 'text-gray-500': forcedIntlSite }">{{ t(':use_intl_site') }} <span class="bg-gray-200 text-gray-900 rounded px-1">www.pathofexile.com</span></ui-checkbox>
+    <div class="mb-4 mt-4">
+      <div class="mb-4" v-if="language === 'zh_CN'">
+        <div class="flex-1 mt-2" v-show="realm === 'pc-tencent'">{{ t('sale_type') }}</div>
+        <div class="flex gap-x-4" v-show="realm === 'pc-tencent'">
+          <ui-radio v-model="saleType" value="any">{{ t('sale_type_any') }}</ui-radio>
+          <ui-radio v-model="saleType" value="strict_auto_buyout">{{ t('sale_type_strict_auto_buyout') }}</ui-radio>
+        </div>
+      </div>
 
-    <div class="mb-4">
-      <div class="flex-1 mb-1">{{ t(':font_size') }}</div>
-      <div class="flex gap-1">
-        <input v-model.number="fontSize" class="rounded bg-gray-900 px-1 block w-16 font-poe text-center" />
-        <span>px</span>
+      <div class="mb-4">
+        <div class="flex-1 mb-1">{{ t(':font_size') }}</div>
+        <div class="flex gap-1">
+          <input v-model.number="fontSize" class="rounded bg-gray-900 px-1 block w-16 font-poe text-center" />
+          <span>px</span>
+        </div>
       </div>
-    </div>
-    <ui-checkbox class="mb-4"
-      v-model="restoreClipboard">{{ t(':restore_clipboard') }}</ui-checkbox>
-    <div class="mb-2">
-      <div class="flex-1 mb-1">{{ t(':poe_log_file') }}</div>
-      <input v-model.trim="clientLog"
-        class="rounded bg-gray-900 px-1 block w-full font-sans" placeholder="...?/Grinding Gear Games/Path of Exile/logs/Client.txt">
-    </div>
-    <div class="mb-4">
-      <div class="flex-1 mb-1">{{ t(':poe_cfg_file') }}</div>
-      <input v-model.trim="gameConfig"
-        class="rounded bg-gray-900 px-1 block w-full font-sans" placeholder="...?/My Games/Path of Exile/production_Config.ini">
-    </div>
-    <hr class="mb-4 mx-8 border-gray-700">
-    <div class="mb-2">
-      <div class="mb-1">{{ t(':overlay_bg') }}</div>
-      <div class="flex gap-4 items-baseline">
-        <input v-model="overlayBackground" class="rounded bg-gray-900 px-1 block w-48 font-poe text-center" />
-        <ui-radio v-model="overlayBackground" value="rgba(255, 255, 255, 0)">{{ t(':overlay_bg_none') }}</ui-radio>
+      <ui-checkbox class="mb-4"
+        v-model="restoreClipboard">{{ t(':restore_clipboard') }}</ui-checkbox>
+      <div class="mb-2">
+        <div class="flex-1 mb-1">{{ t(':poe_log_file') }}</div>
+        <input v-model.trim="clientLog"
+          class="rounded bg-gray-900 px-1 block w-full font-sans" placeholder="...?/Grinding Gear Games/Path of Exile/logs/Client.txt">
       </div>
-    </div>
-    <ui-checkbox class="mb-2" v-if="overlayBackground !== 'rgba(255, 255, 255, 0)'"
-      v-model="overlayBackgroundClose">{{ t(':overlay_bg_focus_game') }}</ui-checkbox>
-    <ui-checkbox class="mb-4"
-      v-model="showAttachNotification">{{ t(':show_overlay_ready') }}</ui-checkbox>
-    <div class="mb-4">
-      <div class="flex-1 mb-1">{{ t(':window_title') }} <span class="bg-gray-200 text-gray-900 rounded px-1">{{ t('Restart required') }}</span></div>
-      <input v-model="windowTitle" class="rounded bg-gray-900 px-1 block w-full mb-1 font-poe" />
+      <div class="mb-4">
+        <div class="flex-1 mb-1">{{ t(':poe_cfg_file') }}</div>
+        <input v-model.trim="gameConfig"
+          class="rounded bg-gray-900 px-1 block w-full font-sans" placeholder="...?/My Games/Path of Exile/production_Config.ini">
+      </div>
+      <hr class="mb-4 mx-8 border-gray-700">
+      <div class="mb-2">
+        <div class="mb-1">{{ t(':overlay_bg') }}</div>
+        <div class="flex gap-4 items-baseline">
+          <input v-model="overlayBackground" class="rounded bg-gray-900 px-1 block w-48 font-poe text-center" />
+          <ui-radio v-model="overlayBackground" value="rgba(255, 255, 255, 0)">{{ t(':overlay_bg_none') }}</ui-radio>
+        </div>
+      </div>
+      <ui-checkbox class="mb-2" v-if="overlayBackground !== 'rgba(255, 255, 255, 0)'"
+        v-model="overlayBackgroundClose">{{ t(':overlay_bg_focus_game') }}</ui-checkbox>
+      <ui-checkbox class="mb-4"
+        v-model="showAttachNotification">{{ t(':show_overlay_ready') }}</ui-checkbox>
+      <div class="mb-4">
+        <div class="flex-1 mb-1">{{ t(':window_title') }} <span class="bg-gray-200 text-gray-900 rounded px-1">{{ t('Restart required') }}</span></div>
+        <input v-model="windowTitle" class="rounded bg-gray-900 px-1 block w-full mb-1 font-poe" />
+      </div>
     </div>
   </div>
 </template>
