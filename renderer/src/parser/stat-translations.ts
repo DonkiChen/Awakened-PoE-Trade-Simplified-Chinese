ac03cdb3d673a58c805d318d2bc03167434dc952
Lucas Chen <l_in1995@163.com>
2026-03-17 04:24:54 +0800

    修复军帽词缀问题

--- renderer/src/parser/stat-translations.ts
+++ renderer/src/parser/stat-translations.ts
@@ -35,7 +35,8 @@ export function * linesToStatStrings (lines: string[]): Generator<StatString, st
 
   outer:
   for (let start = 0; start < lines.length; start += 1) {
-    lines[start].replace(/\(高阶多重投射-专擅反击\)/g, '') // 解决军帽的问题
+    // FIXME: 去掉特化逻辑
+    lines[start] = lines[start].replace(/\(高阶多重投射-圣化\)/g, '') // 解决军帽的问题
     if (lines[start].match(LOCALIZED_PAREN_LEFT) ||
         lines[start] === _$.EXARCH_ITEM ||
         lines[start] === _$.EATER_ITEM) {
